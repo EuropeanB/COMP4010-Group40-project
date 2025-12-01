a34e133ec5834de326d2961089e9e265c863a53a
AlphabetaB <55248830+EuropeanB@users.noreply.github.com>
2025-12-01 15:05:08 -0500

    Add PPOAgent execution

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from RunDQNAgent import RunDQNAgent
 
 if __name__ == "__main__":
     #RunPPOAgent(True, save_directory="Models")
+    #Run ACAgent
+    #RunACAgent(True, save_directory="Models")
+    #Run PPOAgent
     RunPPOAgent(False, test_model="Models/PPO_BestAgent.pth", num_tests=1000, test_render_mode='human')
 
     #RunDQNAgent(True, save_directory='Models')
+
